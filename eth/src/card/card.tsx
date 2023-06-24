a1f3456d122779ef805c554caf9aff16799c829e
christinaly19 <christinayxli@gmail.com>
2023-06-24 16:55:16 -0400

    more shit

---
+++ eth/src/card/card.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../header/header"
+
+export default function Card() {
+    return (
+        <main>
+            <Header></Header>
+        </main>
+    )
+}
---
+++ eth/src/header/header.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function header() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            HEADER
+        </main>
+    )
+}
