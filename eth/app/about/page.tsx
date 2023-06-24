e51c2e2abbd148191bd5982fc820257035d236c3
Ethan Wang <ethanwang1203@gmail.com>
2023-06-24 18:03:56 -0400

    Update about

--- eth/app/about/page.tsx
+++ eth/app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
-    <article className="h-full w-full bg-blue-100">
-      <div className="text-2xl text-white">dsfghtreas</div>
+    <article className="bg-blue-100 min-h-screen w-full">
+      <div className="text-white p-4 text-center text-3xl font-bold">About</div>
     </article>
   );
 }
