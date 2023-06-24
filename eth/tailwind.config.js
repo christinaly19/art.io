f578257edd6dda46b0686d4b6fd575ea8e1dec4e
christinaly19 <christinayxli@gmail.com>
2023-06-24 17:40:26 -0400

    navbar, global colours in tailwind config

--- eth/tailwind.config.js
+++ eth/tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'headerColorRed': '#D53C5E',
+      'secondaryHeaderColorOrange': 'F8C146',
+      'secondaryHeaderColorYellow':'FFF671',
+      'bgColorGray':'D5D7E1',
+      'textColorMidnight':'151C3B'
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
