2faa6cd91f11bf09e00fe91cd2737889a2389ba1
jeessh <jesshuang5733@gmail.com>
2023-06-24 20:41:16 -0400

    edited

---
+++ eth/app/profile/page.tsx
@@ -0,0 +1,21 @@
+import './profile.css';
+
+export default function Profile() {
+    return(
+    <div className='profile-container'>
+        <div className='profile-info'>
+            <img className='pf-photo'/>
+            <div className='pf-text'>
+                <h3>Username</h3>
+                <p>Elnivae</p>
+
+                <h3>Email</h3>
+                <p>awesomethan1026@gmail.com</p>
+            </div>
+        </div>
+        <div className='profile-art'>
+
+        </div>
+    </div>
+    )
+}
